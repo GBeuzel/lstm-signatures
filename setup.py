6801daa410b760b88215dc276c1e4f73cc23b95f
Giel Beuzel <G.Beuzel@bibliotheekeindhoven.nl>
2024-03-21 10:08:52 +0100

    Iniitalized repo with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='LSTM Stego signatures',
+    author='Giel Beuzel',
+    license='MIT',
+)
